17c65ab7d12435cc0cf4bd99ae347cb2b7af1228
matty6223 <39472462+matty6223@users.noreply.github.com>
2021-03-20 14:00:48 +1000

    movement disabled during tractor beam fire.
    Player can no longer move while tractor beam is being used.

---
+++ UFO Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movement_scalar;
+
+    public float max_speed;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+
+        }
+        else
+        {
+            float x_movement = Input.GetAxis("Horizontal");
+            float y_movement = Input.GetAxis("Vertical");
+            if (rb.velocity.magnitude < max_speed)
+            {
+                Vector3 movement = new Vector3(x_movement, y_movement);
+                rb.AddForce(movement_scalar * movement);
+            }
+        }
+
+            
+    }
+
+   
+}
