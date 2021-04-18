9297ce49be161e13a72c8bd9da90bb22937f06e2
Andrew4354 <s3882566@student.rmit.edu.au>
2021-04-18 16:33:23 +1000

    pray to jesus

--- UFO Game/Assets/Scripts/PlayerSound.cs
+++ UFO Game/Assets/Scripts/PlayerSound.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerSound : MonoBehaviour
 {
+    public AudioClip Audio1;
+    public AudioClip Audio2;
+    public AudioClip Audio3;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rb.velocity.magnitude > 0)
+        {
+           
+        }
     }
 }
