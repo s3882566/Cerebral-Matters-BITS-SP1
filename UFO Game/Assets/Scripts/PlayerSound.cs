f93deb604a98e3ce272f3fc8af8917b24147e859
Andrew4354 <s3882566@student.rmit.edu.au>
2021-04-18 19:54:17 +1000

    added even more things

--- UFO Game/Assets/Scripts/PlayerSound.cs
+++ UFO Game/Assets/Scripts/PlayerSound.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerSound : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private AudioSource Audio1;
+    public AudioSource Audio11 { get => Audio1; set => Audio1 = value; }
+    public AudioSource Audio21 { get => Audio2; set => Audio2 = value; }
+
+    private AudioSource Audio2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rb.velocity.magnitude > 0)
+        {
+            // Player is moving
+        }
     }
 }
